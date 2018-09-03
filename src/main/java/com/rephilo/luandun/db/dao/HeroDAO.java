0323fb9856e2357085dffdd4f189ff37edc9e0fd
wangxiao15 <wangxiao15@maoyan.com>
2018-09-03 17:06:32 +0800

    删除无用类

--- src/main/java/com/rephilo/luandun/db/dao/HeroDAO.java
+++
@@ -1,6 +0,0 @@
-package com.rephilo.luandun.db.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeroDAO extends JpaRepository {
-}
--- src/main/java/com/rephilo/luandun/interceptor/FreyaInterceptor.java
+++
@@ -1,6 +0,0 @@
-package com.rephilo.luandun.interceptor;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class FreyaInterceptor extends HandlerInterceptorAdapter {
-}
