dc4992a36c38f8ac36d178216073174d8c171eaf
wangxiao15 <wangxiao15@maoyan.com>
2018-11-30 11:10:20 +0800

    redis配置修改

--- src/main/java/com/rephilo/luandun/config/RedisConfig.java
+++ src/main/java/com/rephilo/luandun/config/RedisConfig.java
@@ -2,11 +2,12 @@ package com.rephilo.luandun.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 /**
  * redis配置类
@@ -16,8 +17,20 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 @Configuration
 @EnableRedisRepositories(basePackages = "com.rephilo.luandun.dao.redis")
 public class RedisConfig {
+
     @Bean
-    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate() {
-        return new ReactiveRedisTemplate<>(new LettuceConnectionFactory("localhost", 6379), RedisSerializationContext.string());
+    public LettuceConnectionFactory redisConnectionFactory() {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().build();
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration("localhost", 6379);
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate() {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
     }
 }
