0ebc29d7c566eec5b2910f90d90ae9ecaa274192
王霄 <wangxiao15@maoyan.com>
2019-02-21 19:00:11 +0800

    栈实现括号验证

---
+++ src/main/java/com/rephilo/luandun/service/datastructure/QueueService.java
@@ -0,0 +1,13 @@
+package com.rephilo.luandun.service.datastructure;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 极简数据结构 第5章
+ *
+ * @author rephilo
+ */
+@Service
+public class QueueService {
+
+}
