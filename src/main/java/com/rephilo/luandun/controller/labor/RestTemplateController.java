17e6dd7fc57bdcae8a4cdf7cfffe516d17fa5921
王霄 <wangxiao15@maoyan.com>
2019-07-21 14:31:54 +0800

    002

--- src/main/java/com/rephilo/luandun/controller/labor/RestTemplateController.java
+++ src/main/java/com/rephilo/luandun/controller/labor/RestTemplateController.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.rephilo.luandun.model.labor.excel.ExcelConstant;
 import com.rephilo.luandun.model.labor.excel.ExcelData;
 import com.rephilo.luandun.model.labor.excel.ExcelUtils;
+import com.sun.jdi.IntegerType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,27 +25,32 @@ public class RestTemplateController {
 
 
         BufferedReader br = null;
-        br = new BufferedReader(new FileReader("aaa.txt"));
+        br = new BufferedReader(new FileReader("dpId.csv"));
 
-        ExcelData data = new ExcelData();
-        List<String> title = Lists.newArrayList();
+//        ExcelData data = new ExcelData();
+//        List<String> title = Lists.newArrayList();
 
-        title.add("realNum");
-        title.add("prodNum");
-        title.add("diff");
+//        title.add("realNum");
+//        title.add("prodNum");
+//        title.add("diff");
 
-        data.setTitles(title);
+//        data.setTitles(title);
 
-        List<List<Object>> rows = Lists.newArrayList();
+//        List<List<Object>> rows = Lists.newArrayList();
         String line;
+        List<Integer> dpIdList = Lists.newArrayList();
         while ((line = br.readLine()) != null) {
             String[] ss = line.split("\\t");
 
-            int subjectId = Integer.parseInt(ss[1]);
-            int num = Integer.parseInt(ss[0]);
+            int dpId = Integer.parseInt(ss[0]);
+
+            dpIdList.add(dpId);
         }
-        data.setRows(rows);
-        ExcelUtils.generateExcel(data, ExcelConstant.FILE_PATH + System.currentTimeMillis() + ExcelConstant.FILE_NAME);
+
+//        dpIdList.parallelStream().forEach(this::refresh);
+//        data.setRows(rows);
+//        ExcelUtils.generateExcel(data, ExcelConstant.FILE_PATH + System.currentTimeMillis() + ExcelConstant.FILE_NAME);
+        System.out.println("finish");
         return "ok";
     }
 }
