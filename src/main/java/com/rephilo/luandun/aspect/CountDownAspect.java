7c0129f2066a0c8ff03b8233e9ed6371aa9a94ae
王霄 <wangxiao15@maoyan.com>
2019-08-15 21:23:08 +0800

    更新一下计时注解

--- src/main/java/com/rephilo/luandun/aspect/CountDownAspect.java
+++ src/main/java/com/rephilo/luandun/aspect/CountDownAspect.java
@@ -1,5 +1,6 @@
 package com.rephilo.luandun.aspect;
 
+import com.google.common.base.Stopwatch;
 import com.rephilo.luandun.model.vo.BaseTimeVO;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -7,6 +8,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 记录时间拦截器实现
@@ -34,12 +37,14 @@ public class CountDownAspect implements Ordered {
     @Around("countDown()")
     public Object countUseTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         //记录开始时间
-        long startTime = System.currentTimeMillis();
+        Stopwatch stopwatch = Stopwatch.createStarted();
         //执行操作
         Object obj = proceedingJoinPoint.proceed();
+
+        stopwatch.stop();
         //设置消耗时间
         if (obj instanceof BaseTimeVO) {
-            ((BaseTimeVO) obj).setTime(System.currentTimeMillis() - startTime);
+            ((BaseTimeVO) obj).setTime(stopwatch.elapsed(TimeUnit.MILLISECONDS));
         }
 
         return obj;
