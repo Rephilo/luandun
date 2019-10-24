e2dfaad1631deda3b7266bee19bd2eebc154325f
王霄 <wangxiao15@maoyan.com>
2019-10-24 19:59:44 +0800

    广度优先查找图中的路径

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -124,4 +124,31 @@ public class LeetcodeService {
             return result.intValue();
         }
     }
+
+    /**
+     * 704
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search704(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    public static int binarySearch(int[] nums, int start, int end, int target) {
+        int mid = (start + end) / 2;
+        if (start > end) {
+            return -1;
+        }
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return binarySearch(nums, mid + 1, end, target);
+        } else if (nums[mid] > target) {
+            return binarySearch(nums, start, mid - 1, target);
+        }
+
+        return -1;
+    }
 }
