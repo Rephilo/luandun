9b040b380f35a6aa135d379106bd424f34cb2466
王霄 <wangxiao15@maoyan.com>
2020-04-09 18:04:21 +0800

    844 first submit

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -460,6 +460,39 @@ public class LeetcodeService {
     }
 
     /**
+     * 844
+     *
+     * @param S
+     * @param T
+     * @return
+     */
+    public boolean backspaceCompare(String S, String T) {
+        StringBuilder s = new StringBuilder();
+        StringBuilder t = new StringBuilder();
+        for (Character c : S.toCharArray()) {
+            if (c.equals('#')) {
+                if (s.length() > 0) {
+                    s.deleteCharAt(s.length() - 1);
+                }
+            } else {
+                s.append(c);
+            }
+        }
+
+        for (Character c : T.toCharArray()) {
+            if (c.equals('#')) {
+                if (t.length() > 0) {
+                    t.deleteCharAt(t.length() - 1);
+                }
+            } else {
+                t.append(c);
+            }
+        }
+
+        return s.toString().equals(t.toString());
+    }
+
+    /**
      * 876
      *
      * @param head
