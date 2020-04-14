e801b1605105845df2465f81b560b5eda82bec1f
王霄 <wangxiao15@maoyan.com>
2020-04-14 10:25:13 +0800

    525 strange question

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -438,6 +438,27 @@ public class LeetcodeService {
     }
 
     /**
+     * 525
+     *
+     * @param nums
+     * @return
+     */
+    public static int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxlen;
+    }
+
+    /**
      * 543
      *
      * @param root
@@ -651,6 +672,6 @@ public class LeetcodeService {
     }
 
     public static void main(String[] args) {
-        lastStoneWeight(new int[]{3, 7, 2});
+        findMaxLength(new int[]{0,0,1,0,0,0,1,1});
     }
 }
