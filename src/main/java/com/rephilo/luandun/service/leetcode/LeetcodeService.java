05c2d85a07ac90102827692122e219c1cf4ab871
王霄 <wangxiao15@maoyan.com>
2020-04-15 15:00:12 +0800

    april 14th

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -671,7 +671,56 @@ public class LeetcodeService {
         return result;
     }
 
+    /**
+     * April 14th
+     *
+     * @param s
+     * @param shift
+     * @return
+     */
+    public static String stringShift(String s, int[][] shift) {
+        int finalDirect = 0;
+        int finalPath = 0;
+
+        for (int i = 0; i < shift.length; i++) {
+            int direct = shift[i][0];
+            int path = shift[i][1];
+            if (direct == 0) {
+                finalPath += path;
+            } else {
+                finalPath -= path;
+            }
+        }
+
+        if (finalPath < 0) {
+            finalDirect = 1;
+            finalPath = Math.abs(finalPath);
+        }
+
+        finalPath = finalPath % s.length();
+
+        char[] newCharArray = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            int cover = i;
+            if (finalDirect == 0) {
+                cover += finalPath;
+            } else {
+                cover -= finalPath;
+            }
+
+            if (cover >= s.length()) {
+                cover -= s.length();
+            } else if (cover < 0) {
+                cover += s.length();
+            }
+
+            newCharArray[i] = s.charAt(cover);
+        }
+
+        return new String(newCharArray);
+    }
+
     public static void main(String[] args) {
-        findMaxLength(new int[]{0,0,1,0,0,0,1,1});
+        stringShift("yisxjwry", new int[][]{{1, 8}, {1, 4}, {1, 3}, {1, 6}, {0, 6}, {1, 4}, {0, 2}, {0, 1}});
     }
 }
