aaa916b95149b95cf71c24fe6f954a7d0a3e7329
874851235@qq.com <123456beijau>
2020-04-18 22:30:03 +0800

    064

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -341,6 +341,37 @@ public class LeetcodeService {
     }
 
     /**
+     * 064
+     * 经典动态规划
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] += dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] += dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+                }
+            }
+        }
+
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+
+    /**
      * 122 只要后一天比前一天高，就可以按照一天一天的买进
      *
      * @param prices
