00838c224fd216a8bc6a5d7f58808a664b4e6db0
874851235@qq.com <123456beijau>
2020-04-29 00:10:58 +0800

    April 28th 用LinkedHashMap凑合做出来了 要是没这个结构怎么办呢

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -1095,4 +1095,60 @@ public class LeetcodeService {
 
         return result;
     }
+
+    /**
+     * April 28th
+     */
+    static class FirstUnique {
+
+        private LinkedHashMap<Integer, Integer> uniqueMap;
+        private Set<Integer> duplicate;
+
+        public FirstUnique(int[] nums) {
+            uniqueMap = new LinkedHashMap<>();
+            duplicate = new HashSet<>();
+
+            for (int num : nums) {
+                handle(num);
+            }
+        }
+
+        private void handle(int num) {
+            //已重复的直接过滤
+            if (duplicate.contains(num)) {
+                return;
+            }
+            //不在唯一表中，则直接加入唯一表
+            if (!uniqueMap.containsKey(num)) {
+                uniqueMap.put(num, num);
+            } else {
+                //在唯一表中，则从唯一表中删除，放入重复表
+                uniqueMap.remove(num);
+                duplicate.add(num);
+            }
+        }
+
+        public int showFirstUnique() {
+            if (uniqueMap.isEmpty()) {
+                return -1;
+            } else {
+                return uniqueMap.entrySet().iterator().next().getKey();
+            }
+        }
+
+        public void add(int value) {
+            handle(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        FirstUnique firstUnique = new FirstUnique(new int[]{2, 3, 5});
+        firstUnique.showFirstUnique(); // return 2
+        firstUnique.add(5);            // the queue is now [2,3,5,5]
+        firstUnique.showFirstUnique(); // return 2
+        firstUnique.add(2);            // the queue is now [2,3,5,5,2]
+        firstUnique.showFirstUnique(); // return 3
+        firstUnique.add(3);            // the queue is now [2,3,5,5,2,3]
+        firstUnique.showFirstUnique(); // return -1
+    }
 }
