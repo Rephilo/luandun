f59db532c7fbfe287cd7e91911c1f5509b871f65
874851235@qq.com <123456beijau>
2020-05-01 22:41:33 +0800

    278 scala

--- src/main/scala/com/rephilo/luandun/leetcode/LeetCode.scala
+++ src/main/scala/com/rephilo/luandun/leetcode/LeetCode.scala
@@ -255,6 +255,37 @@ object LeetCode {
   }
 
   /**
+   * 278
+   *
+   * @param n
+   * @return
+   */
+  def firstBadVersion(n: Int): Int = {
+    val left = 1
+    val right = n
+
+    @scala.annotation.tailrec
+    def calc(left: Int, right: Int): Int = {
+      if (left >= right) {
+        return left
+      } else {
+        val mid = left + (right - left) / 2
+        if (isBadVersion(mid)) {
+          calc(left, mid)
+        } else {
+          calc(mid + 1, right)
+        }
+      }
+    }
+
+    def isBadVersion(n: Int): Boolean = {
+      return false
+    }
+
+    calc(left, right)
+  }
+
+  /**
     * 283
     *
     * @param nums
