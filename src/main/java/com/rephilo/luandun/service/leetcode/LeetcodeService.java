82fabdd1cc23daf760eac5f4cb4787df0e65028b
874851235@qq.com <123456beijau>
2020-05-01 22:27:51 +0800

    278

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -723,6 +723,31 @@ public class LeetcodeService {
     }
 
     /**
+     * 278
+     *
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean isBadVersion(int n) {
+        return false;
+    }
+
+    /**
      * 283 想到了双指针，没想到简单的写法
      *
      * @param nums
