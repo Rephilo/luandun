49bfe49985bc4c1fbd6529069c0a2e149e1dcb83
874851235@qq.com <123456beijau>
2020-05-04 18:47:32 +0800

    476 弱点项位运算

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -807,6 +807,25 @@ public class LeetcodeService {
     }
 
     /**
+     * 476
+     * 位运算确实是弱项，需要再学习
+     *
+     * @param num
+     * @return
+     */
+    public int findComplement(int num) {
+        int temp = num, c = 0;
+        while (temp > 0) {
+            //根据判断条件
+            //二进制右移并赋值给temp，
+            temp >>= 1;
+            //二进制左移之后结果+1 赋值给c
+            c = (c << 1) + 1;
+        }
+        return num ^ c;
+    }
+
+    /**
      * 525
      *
      * @param nums
--- src/main/scala/com/rephilo/luandun/leetcode/LeetCode.scala
+++ src/main/scala/com/rephilo/luandun/leetcode/LeetCode.scala
@@ -295,6 +295,25 @@ object LeetCode {
   }
 
   /**
+   * 476
+   * 不知道为啥执行不下去呀
+   *
+   * @param num
+   * @return
+   */
+  def findComplement(num: Int): Int = {
+    @tailrec
+    def calc(num: Int, c: Int): Int = {
+      num match {
+        case _ if (num <= 0) => num ^ c
+        case _ => calc(num >> 1, (c << 1) + 1)
+      }
+    }
+
+    calc(num, 0)
+  }
+
+  /**
     * 1108
     *
     * @param address
