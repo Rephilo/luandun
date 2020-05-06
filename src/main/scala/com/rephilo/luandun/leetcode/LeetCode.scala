38bf78b66f8cfbe0b19baa40a22d13b913bd617e
王霄 <wangxiao15@maoyan.com>
2020-05-06 17:48:48 +0800

    169 scala

--- src/main/scala/com/rephilo/luandun/leetcode/LeetCode.scala
+++ src/main/scala/com/rephilo/luandun/leetcode/LeetCode.scala
@@ -207,12 +207,15 @@ object LeetCode {
 
   /**
     * 169
+    * 排序法
+    * 排序之后，多余一半的数字一定在中间
     *
     * @param nums
     * @return
     */
   def majorityElement(nums: Array[Int]): Int = {
-
+    val sortedNums = nums.sorted
+    sortedNums(nums.length / 2)
   }
 
   /**
