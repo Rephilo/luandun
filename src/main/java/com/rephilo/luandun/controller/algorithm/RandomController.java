3d5923de36338802acd614790e2365aff8878e57
王霄 <wangxiao15@maoyan.com>
2020-05-06 16:30:41 +0800

    另外一份数据

--- src/main/java/com/rephilo/luandun/controller/algorithm/RandomController.java
+++ src/main/java/com/rephilo/luandun/controller/algorithm/RandomController.java
@@ -1,5 +1,6 @@
 package com.rephilo.luandun.controller.algorithm;
 
+import com.alibaba.excel.EasyExcel;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.rephilo.luandun.model.param.RandomParam;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 随机数接口
@@ -36,39 +39,32 @@ public class RandomController {
     }
 
     public static void main(String[] args) throws Exception {
-        Reader in = new FileReader("C:\\Users\\rephi\\IdeaProjects\\luandun\\src\\main\\resources\\csv\\1588071870854.csv");
-        Iterable<CSVRecord> zhongtai = CSVFormat.EXCEL.withHeader().parse(in);
+        Reader in = new FileReader("/Users/wangxiao15/Desktop/movie_sns_sns_news.csv");
+        Iterable<CSVRecord> meiti = CSVFormat.EXCEL.withHeader().parse(in);
 
-        Reader in2 = new FileReader("C:\\Users\\rephi\\IdeaProjects\\luandun\\src\\main\\resources\\csv\\1588082328509.csv");
-        Iterable<CSVRecord> meiti = CSVFormat.EXCEL.withHeader().parse(in2);
-
-        Map<String, String> meitiMap = Maps.newHashMap();
-        meiti.forEach(x -> meitiMap.put(x.get("content_id"), x.get("subject_type")));
+        Reader in2 = new FileReader("/Users/wangxiao15/Desktop/ID小于30W.csv");
+        Iterable<CSVRecord> zhongtai = CSVFormat.EXCEL.withHeader().parse(in2);
 
         List<String> sys = Lists.newArrayList();
 
+        Set<String> contentIdSet = new HashSet<>();
         for (CSVRecord record : zhongtai) {
             String id = record.get(0);
-            String source = record.get(3);
-
-            String sub = meitiMap.get(id);
-
-            if (sub == null) {
-                sys.add(id);
+            String type = record.get(1);
+            if ("2".equals(type)) {
                 continue;
             }
 
-            if ("101".equals(sub) && "8".equals(source)) {
-                System.out.println(id);
-            }
-
-            if ("0".equals(sub) && "9".equals(source)) {
-                System.out.println(id);
-            }
+            contentIdSet.add(id);
         }
 
-        if(!CollectionUtils.isEmpty(sys)){
-            System.out.println(sys);
-        }
+        meiti.forEach(r -> {
+            String newsId = r.get(0);
+            if (!contentIdSet.contains(newsId)) {
+                sys.add(newsId);
+            }
+        });
+
+        System.out.println(sys);
     }
 }
