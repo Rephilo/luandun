1b021bb115da0b58f6d063c9fae1464ce181f502
王霄 <wangxiao15@maoyan.com>
2020-05-07 16:43:38 +0800

    993 一遇到二叉树就崩。。

--- src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
+++ src/main/java/com/rephilo/luandun/service/leetcode/LeetcodeService.java
@@ -1114,6 +1114,37 @@ public class LeetcodeService {
         return head;
     }
 
+
+    /**
+     * 993
+     * 用两个hashMap存，好秀啊。。
+     *
+     * @param root
+     * @param x
+     * @param y
+     * @return
+     */
+    private Map<Integer, Integer> depthMap;
+    private Map<Integer, TreeNode> parentMap;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        depthMap = new HashMap<>();
+        parentMap = new HashMap<>();
+        dfsIsCousins(root, null);
+        return depthMap.get(x).equals(depthMap.get(y))
+                && !parentMap.get(x).equals(parentMap.get(y));
+    }
+
+    private void dfsIsCousins(TreeNode curr, TreeNode parent) {
+        if (curr == null) {
+            return;
+        }
+        depthMap.put(curr.val, parent != null ? 1 + depthMap.get(parent.val) : 0);
+        parentMap.put(curr.val, parent);
+        dfsIsCousins(curr.left, curr);
+        dfsIsCousins(curr.right, curr);
+    }
+
     /**
      * 1046
      *
