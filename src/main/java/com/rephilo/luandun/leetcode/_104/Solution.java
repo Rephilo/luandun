d97547d47cd301f30fe61cb9f59d1924127efb01
874851235@qq.com <123456beijau>
2020-08-01 23:50:12 +0800

    104

--- src/main/java/com/rephilo/luandun/leetcode/_104/Solution.java
+++ src/main/java/com/rephilo/luandun/leetcode/_104/Solution.java
@@ -2,6 +2,9 @@ package com.rephilo.luandun.leetcode._104;
 
 import com.rephilo.luandun.leetcode.datastructure.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 104
  *
@@ -15,11 +18,41 @@ public class Solution {
      * @param root
      * @return
      */
-    public int maxDepth(TreeNode root) {
+    public int maxDepth1(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        return 1 + Math.max(maxDepth1(root.left), maxDepth1(root.right));
+    }
+
+    /**
+     * 留一个广度优先查找的
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
     }
 }
